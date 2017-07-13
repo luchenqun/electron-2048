c5a32776b9a84e2e29f81dfec7cdb14d0bb60676
luchenqun <luchenqun@juzix.io>
2017-07-13 18:30:59 +0800

    增加Game.vue页面

---
+++ src/renderer/components/Game.vue
@@ -0,0 +1,21 @@
+<template>
+<div id="game">
+    <el-row v-for="r in 4" :key="r">
+        <el-col :span="6" v-for="l in 4" :key="l">
+            <el-card class="box-card">
+                <div class="text item">
+                    {{'列表内容 ' + l }}
+                </div>
+            </el-card>
+        </el-col>
+    </el-row>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
